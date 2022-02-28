83aaf8f37dd9695a539676efb1eab039bc7f4ddd
Cherichenko_Sl <Cherry-slava@yandex.ru>
2022-02-28 04:22:07 +0300

    Finished

--- Task Manager/Program.cs
+++ Task Manager/Program.cs
@@ -1,6 +1,32 @@
 ﻿using System.Diagnostics;
 Process[] process = Process.GetProcesses();
+Console.WriteLine("ID:\tName:");
 for(int i=0; i<process.Length; i++)
 {
-    Console.WriteLine(process[i].ProcessName);
-}    
+    Console.WriteLine($"{process[i].Id}\t{process[i].ProcessName}");
+}
+Console.WriteLine("Укажите имя процесса или его ID, который хотите закрыть:");
+string killProcess = Console.ReadLine();
+if(int.TryParse(killProcess, out int result))
+{
+    for (int i = 0; i < process.Length; i++)
+    {
+        Process current = process[i];
+        if(current.Id == result)
+        {
+            current.Kill();
+        }
+    }
+}
+else
+{
+    for (int i = 0; i < process.Length; i++)
+    {
+        Process current = process[i];
+        if(process[i].ProcessName == killProcess)
+        {
+            current.Kill();
+        }
+    }
+}
+
